c56fecc3737c03d385352830af13ece99644da35
Hugo Thunnissen <hugo.thunnissen@gmail.com>
2018-03-26 16:00:56 +0200

    Add package file and completions wrapper

---
+++ completions/composer.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+##
+# Little wrapper to install https://github.com/sjorek/composer-bash-completion
+# with basher, while still being able to get the latest version.
+
+# shellcheck disable=SC2155
+export COMPOSER_COMPLETION_PHP_SCRIPT="$(basher package-path sjorek/composer-bash-completion)/composer-completion.php"
+
+include sjorek/composer-bash-completion compoers-completion.bash
---
+++ package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+##
+# Basher package file
+##
+
+DEPS='sjorek/composer-bash-completion'
+
+BASH_COMPLETIONS='completions/composer.bash'
